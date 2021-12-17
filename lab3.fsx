0a69f0cf72d8bf57dfddfc2ee7405d20e2f998e3
stop-upiter <56906962+stop-upiter@users.noreply.github.com>
2021-12-17 23:44:24 +0300

    some stuff for task 2

--- lab3.fsx
+++ lab3.fsx
@@ -19,15 +19,42 @@ type FormattedText =
 // short fun: string list to string
 let rdc (l: string list) = List.fold (fun acc elem -> acc + elem) "" l 
 
+// short tagging
+let t (s: string) (tag: string) = "<" + tag + ">" + s + "</" + tag + ">"
+
 // bold tags
-let b (s: string, cond: Boolean) = 
-    if cond then "<b>" + s + "</b>"
+let bold (cond: bool) (s: string) = 
+    if cond then t s "b"
     else s
 
 // italic tags
-let it (s: string, cond: Boolean) = 
-    if cond then "<i>" + s + "</i>"
+let italic (cond: bool, s: string) = 
+    if cond then t s "i"
     else s
+
+// get formatted text
+let text (label: string, isBold: bool, isItalic: bool) = 
+    italic isItalic (bold isBold label)
+    
+// header type number i tags
+let header (label: string, headType: HeaderType) = t label ("h"+ (string headType))
+
+// bullet item tags
+let bullet_items (bl: Content list)(d: int) f = 
+    if bl.IsEmpty then ""
+    else t (rdc (List.map (fun item -> t (f item (d+1)) "li") bl)) "ul"
+    
+// paragraph tags
+let paragraph (p: Content list)(d: int) f = 
+    if p.IsEmpty then ""
+    else rdc (List.map (fun item -> t (f item (d+1))"p") p)
+    
+let rec content_tagger (deep: int) (x: Content) = function
+    | Header (h, ht) -> header (h, ht)
+    | Text (l,b,i)-> text (l,b,i)
+    | BulletItemsList bl -> bullet_items bl deep content_tagger
+    | Paragraphs p -> paragraph p deep content_tagger
+
 
 let format (x : FormattedText) = ...
 
